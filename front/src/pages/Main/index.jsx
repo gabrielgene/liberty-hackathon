b5d6072239ba91738948452bd2725cf594fc5476
Italo Lin <dizzi@MacBook-Air-de-Dizzi.local>
2019-02-15 08:34:13 -0200

    Lino/avatar-q&a-crm

--- front/src/pages/Main/index.jsx
+++ front/src/pages/Main/index.jsx
@@ -1,29 +1,23 @@
-import React from 'react';
-import Box from '../../components/Box';
-import Opportunities from '../../components/Opportunities';
-import Questions from '../../components/Questions';
-import Publication from '../../components/Publication';
+import React from "react";
+import Box from "../../components/Box";
+import Questions from "../../components/Questions";
+import Publication from "../../components/Publication";
+
+import { questions } from "../../mock";
 
 const Main = () => (
-  <div style={{ display: 'flex' }}>
-    <div style={{ width: '30%', paddingRight: 24 }}>
+  <div style={{ display: "flex" }}>
+    <div style={{ width: "50%", paddingRight: 24 }}>
       <Box title="Perguntas e Respostas">
-        {[...Array(20).keys()].map(i => (
-          <Opportunities key={i} />
+        {questions.map((item, i) => (
+          <Questions key={i} question={item.question} answer={item.answer} />
         ))}
       </Box>
     </div>
-    <div style={{ width: '40%', paddingRight: 24 }}>
+    <div style={{ width: "50%", paddingRight: 24 }}>
       <Box title="Publicações">
         {[...Array(20).keys()].map(i => (
           <Publication key={i} />
-        ))}
-      </Box>
-    </div>
-    <div style={{ width: '30%' }}>
-      <Box title="Oportunidades" style={{ width: '30%' }}>
-        {[...Array(20).keys()].map(i => (
-          <Questions key={i} />
         ))}
       </Box>
     </div>
