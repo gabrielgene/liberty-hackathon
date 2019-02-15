d80f5739e16dc78945103d4025019c2160925455
Italo Lin <dizzi@MacBook-Air-de-Dizzi.local>
2019-02-15 12:21:25 -0200

    italo/new-questions

--- front/src/components/Questions/index.jsx
+++ front/src/components/Questions/index.jsx
@@ -4,8 +4,27 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
+
+import classnames from "classnames";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  IconButton,
+  Collapse,
+  Button,
+  List
+} from "@material-ui/core";
+
+import Comment from "../Comment";
+
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChatIcon from "@material-ui/icons/Chat";
 
 const styles = theme => ({
   root: {
@@ -19,35 +38,120 @@ const styles = theme => ({
   },
   paper: {
     marginBottom: 8
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  actions: {
+    display: "flex"
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  button: {
+    margin: theme.spacing.unit,
+    width: "20%"
+  },
+  input: {
+    width: "80%"
+  },
+  form: {
+    marginTop: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
-const Opportunities = ({ classes, question, answer }) => (
-  <Paper elevation={1} className={classes.paper}>
-    <ListItem alignItems="flex-start">
-      <ListItemAvatar>
-        <Avatar
-          alt="Remy Sharp"
-          src="https://material-ui.com/static/images/avatar/1.jpg"
-        />
-      </ListItemAvatar>
-      <ListItemText
-        primary={question}
-        secondary={
-          <React.Fragment>
-            <Typography
-              component="span"
-              className={classes.inline}
-              color="textPrimary"
-            >
-              Ali Connors
-            </Typography>{" "}
-            {answer}
-          </React.Fragment>
-        }
-      />
-    </ListItem>
-  </Paper>
-);
+class Opportunities extends React.Component {
+  state = {
+    isExpanded: false
+  };
+
+  handleExpandClick = () => {
+    this.setState(prevState => ({
+      isExpanded: !prevState.isExpanded
+    }));
+  };
+
+  render() {
+    const { classes, question, likes, comments } = this.props;
+    const { isExpanded } = this.state;
+
+    return (
+      <Card className={classes.paper}>
+        <CardContent>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://material-ui.com/static/images/avatar/1.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText primary={question} />
+          </ListItem>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <Typography variant="caption">{likes}</Typography>
+          <IconButton aria-label="Comentários" onClick={this.handleExpandClick}>
+            <ChatIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: isExpanded
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={isExpanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+          <Divider />
+          <CardContent>
+            <Typography variant="h6">Respostas</Typography>
+            <List>
+              {comments.map((item, i) => (
+                <Comment key={i} author={item.author} content={item.content} />
+              ))}
+            </List>
+            <Divider />
+            <form className={classes.form}>
+              <TextField
+                className={classes.input}
+                label="Resposta"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                className={classes.button}
+              >
+                Comentar
+              </Button>
+            </form>
+          </CardContent>
+        </Collapse>
+      </Card>
+    );
+  }
+}
 
 export default withStyles(styles)(Opportunities);
--- front/src/mock/index.js
+++ front/src/mock/index.js
@@ -2,30 +2,70 @@ export const questions = [
   {
     question:
       "Na estrutura da operação do seguro, são identificados cinco elementos básicos e essenciais do seguro, quais são?",
-    answer: "Risco, segurado, segurador, prêmio e indenização."
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "Esse field nao muda de altura"
+      }
+    ],
+    likes: 24
   },
   {
     question:
       "São disposições inseridas na apólice que ampliam ou restringem parte das disposições constantes das Condições Gerais?",
-    answer: "Condições Especiais."
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "Esse field nao muda de altura"
+      },
+      {
+        author: "Italo Lino",
+        content: "Esse aqui tbm rlx"
+      }
+    ],
+    likes: 224
   },
   {
     question:
       "Como se chama a prestação paga pelo segurado, para a contratação do seguro, que se efetiva com a emissão da apólice por parte da empresa seguradora ?",
-    answer: "Prêmio."
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "gzuis"
+      }
+    ],
+    likes: 20
   },
   {
     question: "Como as garantias ou coberturas podem ser classificadas?",
-    answer: "Básicas, adicionais ou acessórias e especiais."
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "Esse field nao muda de altura"
+      }
+    ],
+    likes: 32
   },
   {
     question:
       "Período de tempo, contado a partir do início de vigência do título, que o titular terá que aguardar para poder receber o valor de resgate. Essa definição refere-se ao:",
-    answer: "Prazo de Carência."
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "Esse field nao muda de altura"
+      }
+    ],
+    likes: 2
   },
   {
     question:
       "Qual o parâmetro que compõe o carregamento comercial destinado a admistração da seguradora?",
-    answer: "Gastos de gestão interna"
+    comments: [
+      {
+        author: "Italo Lino",
+        content: "Esse field nao muda de altura"
+      }
+    ],
+    likes: 90
   }
 ];
