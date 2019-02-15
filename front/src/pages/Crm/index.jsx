5bc8f7749c4acac669ae03692685e5c59ad289e1
GabrielGene <gabrielgene97@gmail.com>
2019-02-15 12:33:58 -0200

    Change crm

--- front/src/pages/Crm/index.jsx
+++ front/src/pages/Crm/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import Box from "../../components/Box";
-import Opportunities from "../../components/Opportunities";
+import React from 'react';
+import Box from '../../components/Box';
+import Opportunities from '../../components/Opportunities';
 
 const Crm = () => (
-  <div style={{ display: "flex" }}>
-    <div style={{ width: "30%" }}>
-      <Box title="Oportunidades" style={{ width: "30%" }}>
+  <div style={{ display: 'flex' }}>
+    <div style={{ width: '30%' }}>
+      <Box title="Oportunidades">
+        {[...Array(20).keys()].map(i => (
+          <Opportunities key={i} />
+        ))}
+      </Box>
+    </div>
+    <div style={{ width: '70%' }}>
+      <Box title="Contatos">
         {[...Array(20).keys()].map(i => (
           <Opportunities key={i} />
         ))}
