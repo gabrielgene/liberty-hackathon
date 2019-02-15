85828c559ddc7f4f09fbf7b4f141fc40c328c523
Rafael Melo <rsmelo_@hotmail.com>
2019-02-15 10:34:47 -0300

    Landing page

--- front/src/pages/Landing/index.jsx
+++ front/src/pages/Landing/index.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import image from '../../assets/landing.png';
 
 const Landing = () => (
-  <React.Fragment>
-    <img style={{ width: '100%' }} src={image} />
-    <map name="imgmap">
-      <area
-        shape="rect"
-        coords="48,341,294,275"
-        href="http://www.example.com/"
-      />
-    </map>
-  </React.Fragment>
+  <div className="landing-wrapper">
+    <img style={{ width: '100%' }} src={image} alt=""/>
+    <a href="https://www.youtube.com" style={{
+      height: '100%',
+      width: '26%',
+      position: 'absolute',
+      top: 0,
+      left: 0
+    }}> </a>
+    <a href="https://www.google.com" style={{
+      height: '100%',
+      width: '74%',
+      position: 'absolute',
+      top: 0,
+      right: 0
+    }}> </a>
+  </div>
 );
 
 export default Landing;
