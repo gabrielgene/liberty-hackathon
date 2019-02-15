b0408d05d92d1440b1e9d9eda5e0e7a49bf21a98
Rafael Melo <rsmelo_@hotmail.com>
2019-02-15 09:49:41 -0200

    News (#5)

---
+++ front/src/pages/Main/assets/news.js
@@ -0,0 +1,80 @@
+import news1 from './new1.jpg';
+import news2 from './new2.png';
+import news3 from './news3.jpg';
+import news4 from './news4.jpg';
+import news5 from './news5.jpg';
+import news6 from './news6.jpg';
+import news7 from './news6.jpeg';
+
+export const news = [
+  {
+    image: news6,
+    title: 'Corretor: saiba fazer a captação de imóveis',
+    text: 'Estar envolvido com o mercado imobiliário significa estar sempre em busca de realizar diversas atividades, uma delas é: captação de imóveis.'
+  },
+  {
+    image: news3,
+    title: 'Marketplace do Facebook: como aliar essa ferramenta ao seu site para imobiliária?',
+    text: 'Você já pensou em vender imóveis com o Facebook? Conecte seu site para imobiliária ao marketplace da rede social e conquiste novos clientes, aumentando seu alcance e seus negócios.'
+  },
+  {
+    image: news4,
+    title: 'Futuro e inovações do mercado imobiliário',
+    text: 'Você já parou pra pensar no futuro do mercado imobiliário? O que está por vir? O que vai mudar e o que vai permanecer? Se ainda não parou, pare agora. É de suma importância acompanhar as inovações do mercado imobiliário.'
+  },
+  {
+    image: news5,
+    title: 'Contato com clientes: melhore a sua comunicação e aumente suas vendas',
+    text: 'As inúmeras facilidades trazidas pela internet fazem com que os vendedores de todas as áreas tenham que se atualizar constantemente. As possibilidades disponíveis para compra e os métodos de realizar vendas precisam ser avaliados e adaptados a todo momento.'
+  },
+  {
+    image: news1,
+    title: 'Spin Selling – saiba como vender mais imóveis',
+    text: 'Para vendas maiores e mais complexas, uma das metodologias mais usadas e procuradas é o Spin Selling. Certamente, esta deve ser usada no mercado imobiliário também!'
+  },
+  {
+    image: news2,
+    title: 'Compreenda termos do mercado imobiliário tecnológico',
+    text: 'Cada vez mais o meio digital se tornou parte essencial do marketing imobiliário. Por isso, novas expressões surgem no dia a dia de um corretor.'
+  },
+  {
+    image: news7,
+    title: 'Como escrever um e-mail imobiliário: acerte nas mensagens',
+    text: 'Afinal, como escrever um e-mail imobiliário? No processo de comunicação, quem é responsável pela mensagem? O receptor ou o emissor?'
+  },
+  {
+    image: news6,
+    title: 'Corretor: saiba fazer a captação de imóveis',
+    text: 'Estar envolvido com o mercado imobiliário significa estar sempre em busca de realizar diversas atividades, uma delas é: captação de imóveis.'
+  },
+  {
+    image: news4,
+    title: 'Futuro e inovações do mercado imobiliário',
+    text: 'Você já parou pra pensar no futuro do mercado imobiliário? O que está por vir? O que vai mudar e o que vai permanecer? Se ainda não parou, pare agora. É de suma importância acompanhar as inovações do mercado imobiliário.'
+  },
+  {
+    image: news3,
+    title: 'Marketplace do Facebook: como aliar essa ferramenta ao seu site para imobiliária?',
+    text: 'Você já pensou em vender imóveis com o Facebook? Conecte seu site para imobiliária ao marketplace da rede social e conquiste novos clientes, aumentando seu alcance e seus negócios.'
+  },
+  {
+    image: news5,
+    title: 'Contato com clientes: melhore a sua comunicação e aumente suas vendas',
+    text: 'As inúmeras facilidades trazidas pela internet fazem com que os vendedores de todas as áreas tenham que se atualizar constantemente. As possibilidades disponíveis para compra e os métodos de realizar vendas precisam ser avaliados e adaptados a todo momento.'
+  },
+  {
+    image: news7,
+    title: 'Como escrever um e-mail imobiliário: acerte nas mensagens',
+    text: 'Afinal, como escrever um e-mail imobiliário? No processo de comunicação, quem é responsável pela mensagem? O receptor ou o emissor?'
+  },
+  {
+    image: news1,
+    title: 'Spin Selling – saiba como vender mais imóveis',
+    text: 'Para vendas maiores e mais complexas, uma das metodologias mais usadas e procuradas é o Spin Selling. Certamente, esta deve ser usada no mercado imobiliário também!'
+  },
+  {
+    image: news2,
+    title: 'Compreenda termos do mercado imobiliário tecnológico',
+    text: 'Cada vez mais o meio digital se tornou parte essencial do marketing imobiliário. Por isso, novas expressões surgem no dia a dia de um corretor.'
+  },
+];
