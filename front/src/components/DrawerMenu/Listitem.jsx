14be9034b69be8731c89db407589bd14ffe8620d
Rafael Melo <rsmelo_@hotmail.com>
2019-02-15 07:30:46 -0200

    tooltip (#4)

--- front/src/components/DrawerMenu/Listitem.jsx
+++ front/src/components/DrawerMenu/Listitem.jsx
@@ -3,16 +3,20 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import { NavLink } from "react-router-dom";
+import Tooltip from '@material-ui/core/Tooltip';
 
 const ListItemMenu = ({to, icon, text}) => {
   return (
     <NavLink to={to} style={{textDecoration:'none'}}>
-      <ListItem style={{ paddingLeft: 24 }} button>
-        <ListItemIcon>
-          {icon}
-        </ListItemIcon>
-        <ListItemText primary={text} />
-      </ListItem>
+      <Tooltip title={text} placement="right" >
+        <ListItem style={{ paddingLeft: 24 }} button>
+          <ListItemIcon>
+            {icon}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      </Tooltip>
+
     </NavLink>
   )
 }
