f6b9ada630a9131bef772a294f80380db0564430
Italo Lin <dizzi@MacBook-Air-de-Dizzi.local>
2019-02-16 11:22:35 -0200

    fix menu for client type

--- front/src/pages/Login/index.jsx
+++ front/src/pages/Login/index.jsx
@@ -1,71 +1,75 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import logo from '../../assets/logo.png';
-import bg from '../../assets/bg.jpg';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import logo from "../../assets/logo.png";
+import bg from "../../assets/bg.jpg";
+
+import { USER_TYPE, TYPE_CORRETOR, TYPE_CLIENTE } from "../../util";
 
 const styles = theme => ({
   holder: {
     backgroundImage: `url(${bg})`,
-    height: '100vh',
-    paddingTop: '20vh',
-    width: '100vw',
+    height: "100vh",
+    paddingTop: "20vh",
+    width: "100vw"
   },
   main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
       width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
   imgLogin: {
-    width: 150,
+    width: 150
   },
   paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`,
+      .spacing.unit * 3}px`
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
   },
   submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
+    marginTop: theme.spacing.unit * 3
+  }
 });
 
 class SignIn extends React.Component {
   state = {
-    email: '',
-    password: '',
+    email: "",
+    password: ""
   };
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onClick = () => {
     const { email, password } = this.state;
-    if (email === 'corretor' && password === '123') {
-      this.props.history.push('/inicio', { type: email });
-    } else if (email === 'cliente' && password === '123') {
-      this.props.history.push('/inicio', { type: email });
-    } else alert('Usuário não encontrado');
+    if (email === "corretor" && password === "123") {
+      this.props.history.push("/inicio");
+      localStorage.setItem(USER_TYPE, TYPE_CORRETOR);
+    } else if (email === "cliente" && password === "123") {
+      this.props.history.push("/inicio");
+      localStorage.setItem(USER_TYPE, TYPE_CLIENTE);
+    } else alert("Usuário não encontrado");
   };
 
   render() {
--- front/src/util/index.js
+++ front/src/util/index.js
@@ -1,2 +1,6 @@
 export const API_DEV_URL = "http://localhost:3000/";
 export const API_PROD_URL = "";
+
+export const USER_TYPE = "USER_TYPE";
+export const TYPE_CORRETOR = "CORRETOR";
+export const TYPE_CLIENTE = "CLIENTE";
