61531d90ff722f8ae0394024c798f475a28c0a47
Italo Lin <dizzi@MacBook-Air-de-Dizzi.local>
2019-02-16 10:07:32 -0200

    real documents

--- front/src/pages/Feed/index.jsx
+++ front/src/pages/Feed/index.jsx
@@ -1,40 +1,16 @@
-import React from 'react';
-import PropsTypes from 'prop-types';
-import DocumentService from '../../services/Document';
-import { withStyles } from '@material-ui/core';
-import Root from '../../components/Root';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import PropTypes from 'prop-types';
-import Paper from '@material-ui/core/Paper';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import PropsTypes from "prop-types";
+import DocumentService from "../../services/Document";
+import { withStyles } from "@material-ui/core";
+import Root from "../../components/Root";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import classnames from 'classnames';
-import {
-  Card,
-  CardContent,
-  CardActions,
-  IconButton,
-  Collapse,
-  Button,
-  List,
-} from '@material-ui/core';
-
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChatIcon from '@material-ui/icons/Chat';
+import DocumentCard from "../../components/DocumentCard";
 
 class Feed extends React.Component {
   state = {
     isLoading: true,
-    documents: [],
+    documents: []
   };
 
   componentDidMount() {
@@ -43,11 +19,13 @@ class Feed extends React.Component {
       .then(documents =>
         this.setState({
           isLoading: false,
-          documents: documents,
-        }),
+          documents: documents
+        })
       )
       .catch(err =>
-        this.setState({ isLoading: false }, () => this.handleError(err)),
+        this.setState({ isLoading: false, documents: [] }, () =>
+          this.handleError(err)
+        )
       );
   }
 
@@ -61,50 +39,18 @@ class Feed extends React.Component {
       <Root>
         <div
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
           }}
         >
           {isLoading ? (
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
               <CircularProgress color="secondary" />
             </div>
           ) : (
-            [...Array(20)].map(document => (
-              <Paper
-                key={document}
-                elevation={0}
-                style={{
-                  marginBottom: 24,
-                  border: '1px solid rgba(0, 0, 0, 0.2)',
-                  width: '70%',
-                }}
-              >
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Teste
-                    </Typography>
-                    <Typography component="p" style={{ overflow: 'hidden' }}>
-                      Teste2
-                    </Typography>
-                  </CardContent>
-                  <CardActions className={classes.actions} disableActionSpacing>
-                    <IconButton aria-label="Add to favorites">
-                      <FavoriteIcon />
-                    </IconButton>
-                    <Typography variant="caption">2</Typography>
-                    <IconButton aria-label="Comentários">
-                      <ChatIcon />
-                    </IconButton>
-                    <Typography variant="caption">10</Typography>
-                    <IconButton aria-label="Share">
-                      <ShareIcon />
-                    </IconButton>
-                  </CardActions>
-                </div>
-              </Paper>
+            documents.map((document, i) => (
+              <DocumentCard key={i} index={i} {...document} />
             ))
           )}
         </div>
@@ -116,7 +62,7 @@ class Feed extends React.Component {
 const styles = theme => ({});
 
 Feed.PropsTypes = {
-  classes: PropsTypes.object.isRequired,
+  classes: PropsTypes.object.isRequired
 };
 
 export default withStyles(styles)(Feed);
